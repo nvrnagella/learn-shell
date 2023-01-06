f4fa8582b07698414abb6223d22828b820622947
IT1089778 <nvrnagella@gmail.com>
2023-01-06 19:41:19 +0530

    special variables for functions

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,6 +1,18 @@
+echo "values from script"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example() {
+  echo "value from function"
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+example
